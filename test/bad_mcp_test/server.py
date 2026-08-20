fb83d10c797baaabe84d6f68b22f17ed075fd846
thbytwo <thbytwo@live.cn>
2026-08-20 13:04:52 +0800

    blablabla

--- test/bad_mcp_test/server.py
+++
@@ -1,9 +0,0 @@
-from mcp.server import MCPServer
-
-mcp = MCPServer("Calculator")
-
-
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers."""
-    return a + b
--- test/file_for_mcpex/calculator2.py
+++
@@ -1,5 +0,0 @@
-from toolkit import mcp
-@mcp.tool()
-def div(a: int, b: int) -> int:
-    """返回两个整数之商"""
-    return a / b
